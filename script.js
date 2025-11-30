c65dba0313e57d293bfdb98f8193a71b32307e52
NoemiCano <noemicanoconesa@gmail.com>
2025-11-30 10:42:17 +0100

    modification of the script link

--- script.js
+++ script.js
@@ -52,3 +52,23 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+
+/*POKEMON SELECTION AND INVENTORY*/
+
+    // Para diferenciar las dos pantallas
+    const pantallaPokemon = document.querySelector('.pantalla__selecPokemon');
+    const pantallaInventario = document.querySelector('.pantalla__selecInvent');
+
+    //Elección de la imagen de los pokemons
+    const pokemons = document.querySelectorAll('.box__selecPokemon__img');
+
+    // Cambio de pantalla al alegir el pokemon
+    pokemons.forEach(pokemon => {
+    pokemon.addEventListener('click', () => {
+        event.preventDefault();
+        pantallaPokemon.style.display = 'none';
+        pantallaInventario.style.display = 'flex';
+    });
+    });
+
